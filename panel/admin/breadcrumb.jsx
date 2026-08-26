e1d7c9ba3c65727e07b2f912f54ea2dbd0f27275
Saeed Nemati <sn.1361@gmail.com>
2026-08-26 10:28:58 +0330

    committed by script

---
+++ panel/admin/breadcrumb.jsx
@@ -0,0 +1,8 @@
+import PartBreadcrumb from 'partBreadcrumb'
+
+export default props => <PartBreadcrumb
+    icon='calendarMonth'
+    name='installments'
+    title='installmentsInstallments'
+    {...props}
+/>
